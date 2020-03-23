7b316696f6cea54f48e8abbe4470fff5825b823c
leovenom <leonardt@oputlook.com>
2020-03-23 21:16:27 +0000

    sound done

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Circle
   class Application < Rails::Application
+  	config.assets.paths << Rails.root.join("app", "assets", "sounds")
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
